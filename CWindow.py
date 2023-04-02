04e1c782e9de4ad4f4b21d3ba28384cd575522b7
Jaume Serrallonga <jserrallonga@gmail.com>
2023-04-02 21:58:10 +0200

    graella de 4 x 4 caselles

--- CWindow.py
+++ CWindow.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtGui import QPainter, QBrush, QColor
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QBrush, QColor, QPen
+from PyQt5.QtCore import Qt, QPointF
 
 class CWindow(QWidget):
     def __init__(self):
@@ -8,7 +8,10 @@ class CWindow(QWidget):
 
         # Set the window properties
         self.setWindowTitle('My Window')
-        self.setGeometry(100, 100, 500, 500)
+        self.width = 500
+        self.height = 500
+
+        self.setGeometry(200, 200, self.width, self.height)
 
     def paintEvent(self, event):
         # Create a painter object
@@ -17,7 +20,17 @@ class CWindow(QWidget):
         # Set the rendering environment
         painter.setRenderHint(QPainter.Antialiasing, True)
         painter.setBrush(QBrush(QColor(255, 0, 0)))
-        painter.setPen(Qt.NoPen)
 
         # Draw a rectangle
         painter.drawRect(50, 50, 400, 400)
+
+        pen = QPen()
+        pen.setWidth(3);
+
+        painter.setPen(pen);
+        
+        for x in range(0, 5):
+            painter.drawLine(QPointF(50 + x * 100, 50), QPointF(50 + x * 100, 450))
+
+        for y in range(0, 5):
+            painter.drawLine(QPointF(50, 50 + y * 100), QPointF(450, 50 + y * 100))
