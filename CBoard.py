88cea2155375a21a33e32e1d450fc04d9515334f
Martí Serrallonga <martiserrallonga@gmail.com>
2023-04-15 08:11:54 +0200

    Refactor: All CBoard parameters as class members

--- CBoard.py
+++ CBoard.py
@@ -4,23 +4,38 @@ from PyQt5.QtGui import QPainter, QBrush, QColor, QPen
 from PyQt5.QtCore import QPointF
 
 class CBoard(CEntity):
-    def Render(self, painter):
-        # Set the rendering environment
-        painter.setRenderHint(QPainter.Antialiasing, True)
-        painter.setBrush(QBrush(QColor(255, 0, 0)))
+    def __init__(self, cols, rows):
+        self.cols = cols
+        self.rows = rows
+        self.margin = 50
 
-        painter.drawRect(50, 50, 400, 400)
+        self.cellWidth = 100
+        self.cellHeight = 100
+        self.cellColor = QColor(255, 0, 0)
+
+    def Render(self, painter):
+        
+        painter.setBrush(QBrush(self.cellColor))
+        
+        for row in range (self.rows):
+            for col in range (self.cols):
+                x = col * self.cellWidth + self.margin
+                y = row * self.cellHeight + self.margin
+                painter.drawRect(x, y, self.cellWidth, self.cellHeight)
 
         pen = QPen()
         pen.setWidth(3);
-
         painter.setPen(pen);
         
-        for x in range(0, 5):
-            painter.drawLine(QPointF(50 + x * 100, 50), QPointF(50 + x * 100, 450))
+        y = self.rows * self.cellHeight + self.margin
+        for col in range(self.cols + 1):
+            x = col * self.cellWidth + self.margin
+            painter.drawLine(QPointF(x, self.margin), QPointF(x, y))
 
-        for y in range(0, 5):
-            painter.drawLine(QPointF(50, 50 + y * 100), QPointF(450, 50 + y * 100))
+        x = self.cols * self.cellWidth + self.margin
+        for row in range(self.rows + 1):
+            y = row * self.cellHeight + self.margin
+            painter.drawLine(QPointF(self.margin, y), QPointF(x, y))
             
 
 
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     window = CWindow("MyWindow", 200, 200, 500, 500)
-    window.AddEntity(CBoard())
+    window.AddEntity(CBoard(4, 4))
     window.AddEntity(CButton(window))
 
     window.show()
