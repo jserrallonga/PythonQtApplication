f3dfa94d560951681be1b00877ebe164102f9dfe
Martí Serrallonga <martiserrallonga@gmail.com>
2023-04-15 08:39:11 +0200

    Refactor: using range parameters: start, stop, step

--- CBoard.py
+++ CBoard.py
@@ -17,10 +17,8 @@ class CBoard(CEntity):
         
         painter.setBrush(QBrush(self.cellColor))
         
-        for row in range (self.rows):
-            for col in range (self.cols):
-                x = col * self.cellWidth + self.margin
-                y = row * self.cellHeight + self.margin
+        for y in range(self.margin, self.rows * self.cellHeight + self.margin, self.cellHeight):
+            for x in range(self.margin, self.cols * self.cellWidth + self.margin, self.cellWidth):
                 painter.drawRect(x, y, self.cellWidth, self.cellHeight)
 
         pen = QPen()
@@ -28,14 +26,12 @@ class CBoard(CEntity):
         painter.setPen(pen);
         
         y = self.rows * self.cellHeight + self.margin
-        for col in range(self.cols + 1):
-            x = col * self.cellWidth + self.margin
+        for x in range (self.margin, (self.cols + 1) * self.cellWidth + self.margin, self.cellWidth):
             painter.drawLine(QPointF(x, self.margin), QPointF(x, y))
 
         x = self.cols * self.cellWidth + self.margin
-        for row in range(self.rows + 1):
-            y = row * self.cellHeight + self.margin
+        for y in range (self.margin, (self.rows + 1) * self.cellHeight + self.margin, self.cellHeight):
             painter.drawLine(QPointF(self.margin, y), QPointF(x, y))
-            
+
 
 
